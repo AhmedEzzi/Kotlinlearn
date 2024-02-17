1ee84007ce461493d6c190e0081833c8e93939b4
Ahmed Ezz <100492004+AhmedEzzi@users.noreply.github.com>
2024-02-17 05:26:20 +0200

    V.2
    Updated the logic and handelled some cases senaros

--- App1/src/Main.kt
+++ App1/src/Main.kt
@@ -1,32 +1,60 @@
-//TIP Press <shortcut raw="SHIFT"/> twice to open the Search Everywhere dialog and type <b>show whitespaces</b>,
-// then press <shortcut raw="ENTER"/>. You can now see whitespace characters in your code.
-fun main() {
-    val languages = arrayOf("English","German","French","Italino")
-    var known =0
-    var not =0
-    var answer=""
-    repeat(languages.size)
- {
-        println("Do you speak ${languages[it]}")
-     answer = readln()
-     if (languages[it]=="English"&&answer == "yes"){
+fun main()
+{
+val languages = arrayOf("English","Arabic","German","French","Japanese")
+val accent = arrayOf("American English", "British English")
+var known:Byte = 0
+var not:Byte = 0
+var accentNo=0
 
-         println("Do you speak British")
-          answer = readln()
-         println("Do you speak American")
-         answer = readln()
-     }
-     if(answer=="yes")
-         known++
-     else if (answer == "no")
-         not++
-     else
-         println(" Wrong input dude")
- }
-    println("Yes : $known")
-    println( "No : $not")
+for (langu in languages) {
+    println("Do you speak $langu")
+    val answer= readln()
+    if (answer=="yes")
+    {
+        known++
+        var tryAgain = 2
+        if (langu=="English")
+        {
 
-println(" Your rate  is Yes : ${known*100/languages.size}")
-    println("No :${not*100/languages.size} ")
+            do{
+                for (aces in accent) {
+                    println("Do you speak $aces")
+                    val accAnswer = readln()
+                    if (accAnswer == "yes") {
+                        break
+                    } else if (accAnswer == "no")
+                    {accentNo++}
+                    val x= accent.size
+                    if (accentNo == x) {
+                        accentNo=0
+                        tryAgain--
+
+                    }
+
+                }
+if (tryAgain==0)
+    accentNo=accent.size
+            } while (tryAgain>0)
+
+            if(accentNo==accent.size)
+            {
+                known--
+                not++
+            }
+        }
+    }
+    else if(answer=="no")
+    {
+        not++
+    }
+    else{
+        println("Wrong answer dude")
+    }
+
+}
+println("Your Yes : $known and no : $not" )
+
+
+
 
 }
